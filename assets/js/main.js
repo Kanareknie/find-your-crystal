5ec667fee513c58ca875c04ab801b798f0d2a77f
kimneskatarzyna@gmail.com <your_github_email@example.com>
2026-01-01 21:13:29 +0000

    Add API js for Moon phase

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,43 +54,107 @@ async function localDailyQuote() {
   const storageKey = "dailyQuote-" + getTodayKey();
 
   // Check if todays key already exists
-const saved = localStorage.getItem(storageKey);
-if (saved) {
-  const data = JSON.parse(saved);
-  quoteEl.textContent = data.quote;
-  authorEl.textContent = data.author;
-  return;
+  const saved = localStorage.getItem(storageKey);
+  if (saved) {
+    const data = JSON.parse(saved);
+    quoteEl.textContent = data.quote;
+    authorEl.textContent = data.author;
+    return;
 
-}
-// If not, fetch new quote from API
-try { 
-const response = await fetch(API_URL);
-if (!response.ok) {
-  throw new Error(`HTTP error! status: ${response.status}`);
-}
-// JSON response
-const data = await response.json();
+  }
+  // If not, fetch new quote from API
+  try {
+    const response = await fetch(API_URL);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    // JSON response
+    const data = await response.json();
 
-// Prepare daily quote object
-const daily = {
-  quote: data.quote,
-  author: data.author
-};
+    // Prepare daily quote object
+    const daily = {
+      quote: data.quote,
+      author: data.author
+    };
 
-// Save to localStorage
-localStorage.setItem(storageKey, JSON.stringify(daily));
+    // Save to localStorage
+    localStorage.setItem(storageKey, JSON.stringify(daily));
 
-// Update DOM
-quoteEl.textContent = daily.quote;
-authorEl.textContent = daily.author;
+    // Update DOM
+    quoteEl.textContent = daily.quote;
+    authorEl.textContent = daily.author;
 
-} catch (error) {
-  console.error("Error fetching the quote:", error);
-  quoteEl.textContent = "Could not load quote.";
-  authorEl.textContent = "";
-  return;
-}
+  } catch (error) {
+    console.error("Error fetching the quote:", error);
+    quoteEl.textContent = "Could not load quote.";
+    authorEl.textContent = "";
+    return;
+  }
 }
 
 // Call the function to load the quote
 localDailyQuote();
+
+
+// API Moon Phase Fetch
+
+//API URL
+const MOON_API_URL = "https://api.farmsense.net/v1/moonphases/?d=UNIX_TIMESTAMP";
+
+//uNIX_TIMESTAMP replacement -source:  Mozilla.org - Global Objects - Date 
+ function getTodayMiddayUnix() {
+  const now = new Date();
+  const midday = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate(),
+    12, 0, 0
+  );
+  return Math.floor(midday.getTime() / 1000);
+}
+
+//Elements where the API will go
+const moonPhase = document.getElementById("moon-quote");
+
+// Main moon phase function
+async function moonPhaseDaily() {
+
+  const MoonStorageKey = "moon-quote-" + getTodayKey();
+
+  // Check if todays key already exists
+  const savedMoon = localStorage.getItem(MoonStorageKey);
+  if (savedMoon) {
+    const data = JSON.parse(savedMoon);
+    moonPhase.textContent = data[0].Phase;
+    return;
+  }
+  // If not, fetch new moon phase from API
+  try {
+
+    // Replace UNIX_TIMESTAMP in URL
+    const unix = getTodayMiddayUnix();
+    const url = "https://api.farmsense.net/v1/moonphases/?d=UNIX_TIMESTAMP".replace("UNIX_TIMESTAMP", unix);
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    const moonDaily = {
+      phase: data[0].Phase
+    };
+
+    localStorage.setItem(MoonStorageKey, JSON.stringify(moonDaily));
+
+    moonPhase.textContent = moonDaily.phase;
+
+  } catch (error) {
+    console.error("Error fetching the moon phase:", error);
+    moonPhase.textContent = "Could not load moon phase.";
+    return;
+  }
+}
+
+moonPhaseDaily();
+
+
